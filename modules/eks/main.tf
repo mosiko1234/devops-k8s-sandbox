b14864fd7fd405682a806da8df5637287a5cfcf6
danielp <dpahima98@gmail.com>
2024-11-17 09:02:14 +0200

    add role to node group

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -77,3 +77,8 @@ resource "aws_iam_role_policy_attachment" "eks_node_AmazonEC2ContainerRegistryRe
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.eks_node_role.name
 }
+
+resource "aws_iam_role_policy_attachment" "eks_node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
